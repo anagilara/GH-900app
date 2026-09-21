aea38dd85f6246b4b3b1743a0a8741099c7648b9
Ana Gil <anagillara@microsoft.com>
2026-09-21 10:14:04 -0600

    test 2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ghRepo.Models;
+//test 2
 
 namespace ghRepo.Controllers;
 
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 //test
+//test 2
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
