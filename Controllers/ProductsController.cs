e0d79d6b52741adb0a075b8991c522a0f19efeb6
Ana Gil <anagillara@microsoft.com>
2026-09-22 16:11:37 -0600

    test merge 2

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 //test squash and merge 1
 //test squash and merge 2
 //test merge 1
+//test merge 2
 namespace ghRepo.Controllers;
 public class ProductsController : Controller
 {
